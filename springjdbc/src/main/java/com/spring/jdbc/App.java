e09e41ea7c9607d7d64c02b2fb8139afb240bee9
excellarate <excellarate@SYNLPT161>
2022-03-24 18:20:34 +0530

    commitfirst

---
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -0,0 +1,47 @@
+package com.spring.jdbc;
+
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.dao.StudentDao;
+//import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entities.Student;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+		// JdbcTemplate template = context.getBean("jdbctemplate",JdbcTemplate.class);
+
+		//
+		// int result = template.update(query,4,"kiran","pune");
+		// System.out.println("number of record inserted:"+result);
+		StudentDao studentDao = context.getBean("studentdao", StudentDao.class);
+//        Student student=new Student();
+//        student.setId(666);
+//        student.setName("john");
+//        student.setCity("delhi");
+//////        
+//     int result = studentDao.insert(student);  
+//      System.out.println("student added:"+result);
+//       
+//       Student student=new Student();
+//       student.setId(666);
+//       student.setName("jack");
+//       student.setCity("london");
+//       
+//       int result = studentDao.change(student);
+//       System.out.println("updated record:"+result);
+		System.out.println("enter number id for delete");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Student student = new Student();
+		sc.close();
+		int result = studentDao.delete(n);
+		System.out.println("record deleted:" + result);
+	}
+}
